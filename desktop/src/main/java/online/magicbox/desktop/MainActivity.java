4e4e2dd0d9fb71ce67ab9b1d9f68733d49287c69
george.yang <georgeyang1024@gmail.com>
2016-05-27 20:23:38 +0800

    添加提示

--- desktop/src/main/java/online/magicbox/desktop/MainActivity.java
+++ desktop/src/main/java/online/magicbox/desktop/MainActivity.java
@@ -3,6 +3,7 @@ package online.magicbox.desktop;
 import android.app.Activity;
 import android.content.Intent;
 import android.os.Bundle;
+import android.widget.Toast;
 
 import online.magicbox.lib.PluginActivity;
 
@@ -13,9 +14,12 @@ public class MainActivity extends Activity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
 
-        PluginActivity.init("online.magicbox.desktop","magicbox");
+
+        PluginActivity.init("online.magicbox.desktop","magicbox","2");
         Intent intent = PluginActivity.buildIntent(this,MainSlice.class);
         startActivity(intent);
+
+//        Toast.makeText(this,"请用魔盒app安装打开!",Toast.LENGTH_LONG).show();
 
         finish();
     }
--- qrcode/src/main/java/cn/georgeyang/qrcode/MainActivity.java
+++ qrcode/src/main/java/cn/georgeyang/qrcode/MainActivity.java
@@ -3,6 +3,7 @@ package cn.georgeyang.qrcode;
 import android.app.Activity;
 import android.content.Intent;
 import android.os.Bundle;
+import android.widget.Toast;
 
 import online.magicbox.lib.PluginActivity;
 
@@ -12,8 +13,12 @@ public class MainActivity extends Activity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
 
-        PluginActivity.init("cn.georgeyang.qrcode","magicbox");
-        Intent intent = PluginActivity.buildIntent(this,MainSlice.class);
-        startActivity(intent);
+        Toast.makeText(this,"请用魔盒app安装打开!", Toast.LENGTH_LONG).show();
+
+        PluginActivity.init("cn.georgeyang.qrcode","magicbox","1");
+//        Intent intent = PluginActivity.buildIntent(this,MainSlice.class);
+//        startActivity(intent);
+
+        finish();
     }
 }
